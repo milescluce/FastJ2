297c5d2d8f9dcad0bcab7bfbc4855aee593a728c
genderlesspit <59433707+genderlesspit@users.noreply.github.com>
2025-08-24 20:45:00 -0500

    0.1.14 - js and css concatenation

--- test.py
+++ test.py
@@ -6,4 +6,3 @@ from loguru import logger as log
 if __name__ == "__main__":
     j2 = FastJ2("other_file.html", cwd=Path.cwd() / "tests")
     j2.safe_render("null.html", foo = "bar")
-    log.debug(j2.safe_render_string(""))
